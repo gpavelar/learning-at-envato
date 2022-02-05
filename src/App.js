5230cb118166b23724e3511fc087dbc8075f3e94
Gustavo de Paula Avelar <gustavo.avelar@envato.com>
2022-02-05 13:58:27 +1030

    Add tests for App component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
+import React, { useState, useEffect } from 'react';
+import { createBrowserHistory } from 'history';
+
 import logo from './logo.svg';
 import './App.css';
+import Home from './Home.js';
+import Pastel from './Pastel';
+import Feijoada from './Feijoada';
 
 function App() {
+  const [path, setPath] = useState('');
+  const [history, setHistory] = useState();
+
+  const pastelFood = () => {
+    setPath('pastel');
+    history.push('/pastel');
+  };
+
+  const feijoadaFood = () => {
+    setPath('feijoada');
+    history.push('/feijoada');
+  };
+
+  useEffect(() => {
+    setHistory(createBrowserHistory());
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2 id="foods-heading">Foods</h2>
+      {/* <ul aria-label="foods"> */}
+      <ul aria-labelledby="foods-heading">
+        <li>
+          <a onClick={() => pastelFood()}> Pastel food </a>
+        </li>
+        <li>
+          <a onClick={() => feijoadaFood()}> Feijoada food </a>
+        </li>
+      </ul>
+      Top 10 Eaten Food
+      <ul>
+        <li>Sugar cane</li>
+        <li>Strogonoff</li>
+        <li>Testing</li>
+      </ul>
+      {path == 'pastel' && <Pastel />}
+      {path == 'feijoada' && <Feijoada />}
     </div>
   );
 }
